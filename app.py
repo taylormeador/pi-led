c0e3e20a6f862f2847d27b9f2ff497ee4f1838d5
taylormeador <taylor.r.meador@gmail.com>
2021-12-31 13:58:01 -0700

    procfile and pipfile

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import os
+
+LOCAL = True
 
 app = Flask(__name__)
 
@@ -7,4 +10,9 @@ def index():
     return render_template("index.html")
 
 
-app.run()
+# test locally or deploy
+if LOCAL:
+    app.run(port=500)
+else:
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
