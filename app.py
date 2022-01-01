9ffb510c31ea2433a1e76063fbba3a066d4c793e
taylormeador <taylor.r.meador@gmail.com>
2021-12-31 19:16:08 -0700

    implement session logic to keep users logged in

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import bcrypt
@@ -7,11 +7,11 @@ import os
 LOCAL = False
 
 app = Flask(__name__)
-DATABASE_URL = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.secret_key = "secret"
 db = SQLAlchemy(app)
 
-
+# user db model
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
@@ -27,11 +27,15 @@ def index():
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
-def login_page():
+def login():
+    if "username" in session:  # if they are already logged in
+        username = session["username"]
+        return redirect(url_for("user"))
+
     return render_template("login.html")
 
 @app.route("/loginauth", methods=["GET", "POST"])
-def login_auth_page():
+def login_auth():
     # get username/password from form and check against db
     username = request.form.get("username")
     password = request.form.get("password").encode("utf-8")
@@ -42,14 +46,23 @@ def login_auth_page():
             return render_template("login.html", message="wrong username or password, try again")
     else:
         return render_template("login.html", message="wrong username or password, try again")
+
     return render_template("loginauth.html")
 
+@app.route("/user", methods=["GET", "POST"])
+def user():
+    if "username" in session:
+        username = session["username"]
+        return f"<h1>{username}</h1>"
+    else:
+        return redirect(url_for("login"))
+
 @app.route("/register", methods=["GET", "POST"])
-def register_page():
+def register():
     return render_template("register.html")
 
 @app.route("/registerauth", methods=["GET", "POST"])
-def register_auth_page():
+def register_auth():
     desired_username = request.form.get("username")
     desired_password = request.form.get("password")
     """
@@ -84,6 +97,11 @@ def register_auth_page():
 
     # redirect to new page
     return render_template("registerauth.html")
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
 
 
 if LOCAL:
